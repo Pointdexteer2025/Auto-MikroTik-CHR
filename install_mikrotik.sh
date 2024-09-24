e2afda3153b7c6f7e97e0f7e66222277c50cc8be
Dexteer <155999135+Pointdexteer2025@users.noreply.github.com>
2024-09-24 01:09:54 -0700

    Update install_mikrotik.sh

--- install_mikrotik.sh
+++ install_mikrotik.sh
@@ -2,14 +2,14 @@
 
 # Function to display loading animation
 show_loading() {
-    local pid=$!
+    local pid=$1
     local delay=0.1
     local spinstr='|/-\'
     echo -n " "
-    while [ "$(ps a | awk '{print $1}' | grep $pid)" ]; do
+    while ps -p $pid > /dev/null 2>&1; do
         local temp=${spinstr#?}
         printf " [%c]  " "$spinstr"
-        local spinstr=$temp${spinstr%"$temp"}
+        spinstr=$temp${spinstr%"$temp"}
         sleep $delay
         printf "\b\b\b\b\b\b"
     done
@@ -46,47 +46,49 @@ echo -e "\e[33m                                                                 
 echo -e "\e[33m                                                                                         |___/             |__/      \e[0m"
 echo -e "\e[33m                                                                                                                \e[0m"
 
-
 # Check if the user is root
 check_root
 
 # Show system details
 show_system_details
 
+# Install unzip
 echo -e "\e[34mPreparation ...\e[0m" # Blue color
-{
-    apt install unzip -y > /dev/null 2>&1
-} & show_loading
+apt install unzip -y > /dev/null 2>&1 &
+show_loading $!
 
 # Latest Stable
 CHR_VERSION=6.49.13
 
-# Environment
-DISK=$(lsblk | grep "disk" | head -n 1 | cut -d' ' -f1)
+# Environment variables
+DISK=$(lsblk | grep "disk" | head -n 1 | awk '{print $1}')
 INTERFACE=$(ip -o -4 route show to default | awk '{print $5}')
-INTERFACE_IP=$(ip addr show $INTERFACE | grep global | cut -d' ' -f 6 | head -n 1)
+INTERFACE_IP=$(ip addr show $INTERFACE | grep global | awk '{print $2}' | head -n 1)
 INTERFACE_GATEWAY=$(ip route show | grep default | awk '{print $3}')
 
-{
-    wget -qO routeros.zip https://download.mikrotik.com/routeros/$CHR_VERSION/chr-$CHR_VERSION.img.zip && \
-    unzip routeros.zip > /dev/null 2>&1 && \
-    rm -rf routeros.zip
-} & show_loading
+# Download and unzip RouterOS
+wget -qO routeros.zip https://download.mikrotik.com/routeros/$CHR_VERSION/chr-$CHR_VERSION.img.zip && \
+unzip routeros.zip > /dev/null 2>&1 && \
+rm -rf routeros.zip &
+show_loading $!
 
-{
-    mount -o loop,offset=512 chr-$CHR_VERSION.img /mnt > /dev/null 2>&1
-} & show_loading
+# Mount the image
+mount -o loop,offset=512 chr-$CHR_VERSION.img /mnt > /dev/null 2>&1 &
+show_loading $!
 
+# Create autorun script
 echo "/ip address add address=${INTERFACE_IP} interface=[/interface ethernet find where name=ether1]
 /ip route add gateway=${INTERFACE_GATEWAY}
 " > /mnt/rw/autorun.scr
 
-{
-    umount /mnt > /dev/null 2>&1
-    echo u > /proc/sysrq-trigger
-    dd if=chr-$CHR_VERSION.img of=/dev/${DISK} bs=1M > /dev/null 2>&1
-} & show_loading
+# Unmount and write to disk
+umount /mnt > /dev/null 2>&1 &&
+echo u > /proc/sysrq-trigger &&
+dd if=chr-$CHR_VERSION.img of=/dev/${DISK} bs=1M > /dev/null 2>&1 &
+show_loading $!
 
-echo -e "\e[32mInstallation complete. Reboot your server now, Please log in and configure your password using Winbox.\e[0m" # Green color
+# Completion message
+echo -e "\e[32mInstallation complete. Reboot your server now, and configure your password using Winbox.\e[0m" # Green color
 
+# Reboot the system
 reboot

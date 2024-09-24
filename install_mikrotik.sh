78ccbae002ee5a4e2189cce991da1cc6d3a40dee
Dexteer <155999135+Pointdexteer2025@users.noreply.github.com>
2024-09-24 00:20:17 -0700

    Update install_mikrotik.sh

--- install_mikrotik.sh
+++ install_mikrotik.sh
@@ -1,53 +1,90 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-# Automatically detect network interface
+# Function to display loading animation
+show_loading() {
+    local pid=$!
+    local delay=0.1
+    local spinstr='|/-\'
+    echo -n " "
+    while [ "$(ps a | awk '{print $1}' | grep $pid)" ]; do
+        local temp=${spinstr#?}
+        printf " [%c]  " "$spinstr"
+        local spinstr=$temp${spinstr%"$temp"}
+        sleep $delay
+        printf "\b\b\b\b\b\b"
+    done
+    echo " "
+}
+
+# Function to check for root access
+check_root() {
+    if [[ $EUID -ne 0 ]]; then
+        echo -e "\e[31mThis script must be run as root\e[0m" # Red color
+        exit 1
+    fi
+}
+
+# Function to display system details
+show_system_details() {
+    echo -e "\e[34mGathering system details...\e[0m" # Blue color
+    IP=$(curl -s http://checkip.amazonaws.com)
+    RAM=$(free -m | awk '/Mem:/ { print $2 }')
+    CPU=$(lscpu | grep 'Model name' | cut -d: -f2 | xargs)
+    STORAGE=$(df -h | awk '$NF=="/"{printf "%s", $2}')
+    echo -e "\e[32mSystem Details:\nIP: $IP\nRAM: ${RAM}MB\nCPU: $CPU\nStorage: $STORAGE\e[0m" # Green color
+}
+
+# ASCII Banner
+echo -e "\e[33m  														                                                                                        \e[0m"
+echo -e "\e[33m   __  __ _ _           _______ _ _      _____           _        _ _             _              _____       _       \e[0m"
+echo -e "\e[33m  |  \/  (_) |         |__   __(_) |    |_   _|         | |      | | |           | |            / ____|     (_)      \e[0m"
+echo -e "\e[33m  | \  / |_| | ___ __ ___ | |   _| | __   | |  _ __  ___| |_ __ _| | | ___ _ __  | |__  _   _  | (___   __ _ _ _   _ \e[0m"
+echo -e "\e[33m  | |\/| | | |/ / '__/ _ \| |  | | |/ /   | | | '_ \/ __| __/ _` | | |/ _ \ '__| | '_ \| | | |  \___ \ / _` | | | | |\e[0m"
+echo -e "\e[33m  | |  | | |   <| | | (_) | |  | |   <   _| |_| | | \__ \ || (_| | | |  __/ |    | |_) | |_| |  ____) | (_| | | |_| |\e[0m"
+echo -e "\e[33m  |_|  |_|_|_|\_\_|  \___/|_|  |_|_|\_\ |_____|_| |_|___/\__\__,_|_|_|\___|_|    |_.__/ \__, | |_____/ \__,_| |\__,_|\e[0m"
+echo -e "\e[33m                                                                                         __/ |             _/ |      \e[0m"
+echo -e "\e[33m                                                                                         |___/             |__/      \e[0m"
+echo -e "\e[33m															                                                                                        \e[0m"
+
+
+# Check if the user is root
+check_root
+
+# Show system details
+show_system_details
+
+echo -e "\e[34mPreparation ...\e[0m" # Blue color
+{
+    apt install unzip -y > /dev/null 2>&1
+} & show_loading
+
+# Latest Stable
+CHR_VERSION=6.49.13
+
+# Environment
+DISK=$(lsblk | grep "disk" | head -n 1 | cut -d' ' -f1)
 INTERFACE=$(ip -o -4 route show to default | awk '{print $5}')
-echo "Detected network interface: $INTERFACE"
+INTERFACE_IP=$(ip addr show $INTERFACE | grep global | cut -d' ' -f 6 | head -n 1)
+INTERFACE_GATEWAY=$(ip route show | grep default | awk '{print $3}')
 
-# Automatically get IP address for the detected interface
-ADDRESS=$(ip -4 addr show $INTERFACE | grep -oP '(?<=inet\s)\d+(\.\d+){3}/\d+')
-echo "Detected IP address: $ADDRESS"
+{
+    wget -qO routeros.zip https://download.mikrotik.com/routeros/$CHR_VERSION/chr-$CHR_VERSION.img.zip && \
+    unzip routeros.zip > /dev/null 2>&1 && \
+    rm -rf routeros.zip
+} & show_loading
 
-# Automatically detect gateway
-GATEWAY=$(ip route | grep default | awk '{print $3}')
-echo "Detected gateway: $GATEWAY"
+{
+    mount -o loop,offset=512 chr-$CHR_VERSION.img /mnt > /dev/null 2>&1
+} & show_loading
 
-# Automatically detect the primary storage device
-STORAGE_DEVICE=$(lsblk -o NAME,TYPE | grep disk | awk 'NR==1{print "/dev/"$1}')
-echo "Detected storage device: $STORAGE_DEVICE"
+echo "/ip address add address=${INTERFACE_IP} interface=[/interface ethernet find where name=ether1]
+/ip route add gateway=${INTERFACE_GATEWAY}
+" > /mnt/rw/autorun.scr
 
-# Set root password
-ROOT_PASSWORD="your_secure_password_here"
-echo "Using root password: $ROOT_PASSWORD"
+{
+    umount /mnt > /dev/null 2>&1
+    echo u > /proc/sysrq-trigger
+    dd if=chr-$CHR_VERSION.img of=/dev/${DISK} bs=1M > /dev/null 2>&1
+} & show_loading
 
-# Download MikroTik RouterOS image
-wget https://download.mikrotik.com/routeros/7.5/chr-7.5.img.zip -O chr.img.zip && \
-
-# Extract the image
-gunzip -c chr.img.zip > chr.img && \
-
-# Set up loop device
-LOOP_DEVICE=$(sudo losetup -f)
-sudo losetup -fP chr.img && \
-
-# Mount the image
-sudo mount -o loop,offset=512 $LOOP_DEVICE /mnt && \
-
-# Create the MikroTik configuration script
-echo "/ip address add address=$ADDRESS interface=[/interface ethernet find where name=ether1]
-/ip route add gateway=$GATEWAY
-/ip service disable telnet
-/user set 0 name=root password=$ROOT_PASSWORD" > /mnt/rw/autorun.scr && \
-
-# Sync and install RouterOS on the detected storage device
-echo u > /proc/sysrq-trigger && \
-dd if=chr.img bs=1024 of=$STORAGE_DEVICE && \
-echo "sync disk" && \
-echo s > /proc/sysrq-trigger && \
-echo "Sleep 5 seconds" && \
-sleep 5 && \
-echo "Ok, reboot" && \
-echo b > /proc/sysrq-trigger
-
-# Clean up loop device
-sudo losetup -d $LOOP_DEVICE
+echo -e "\e[32mInstallation complete. Reboot your server now, Please log in and configure your password using Winbox.\e[0m" # Green color

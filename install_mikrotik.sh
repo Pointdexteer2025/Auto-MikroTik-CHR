f1ebcee39fe56e0def71fb22a42617731b79c8a1
Dexteer <155999135+Pointdexteer2025@users.noreply.github.com>
2024-09-23 21:53:01 -0700

    Update install_mikrotik.sh

--- install_mikrotik.sh
+++ install_mikrotik.sh
@@ -1,40 +1,46 @@
 #!/bin/bash
 
-# Update and install required packages
-sudo apt-get update
-sudo apt-get install -y qemu-utils qemu-kvm libvirt-bin virtinst bridge-utils
+# Automatically detect network interface (the first non-loopback interface)
+INTERFACE=$(ip -o -4 route show to default | awk '{print $5}')
+echo "Detected network interface: $INTERFACE"
 
-# Set the MikroTik CHR version and download URL
-CHR_VERSION="stable"
-CHR_IMAGE_URL="https://download.mikrotik.com/routeros/$CHR_VERSION/chr-$CHR_VERSION.img.zip"
+# Automatically get IP address for the detected interface
+ADDRESS=$(ip -4 addr show $INTERFACE | grep -oP '(?<=inet\s)\d+(\.\d+){3}/\d+')
+echo "Detected IP address: $ADDRESS"
 
-# Download the MikroTik CHR image
-echo "Downloading MikroTik CHR image..."
-wget $CHR_IMAGE_URL -O chr.img.zip
+# Automatically detect gateway
+GATEWAY=$(ip route | grep default | awk '{print $3}')
+echo "Detected gateway: $GATEWAY"
 
-# Unzip the image
-echo "Unzipping the CHR image..."
-unzip chr.img.zip
+# Automatically detect the primary storage device
+STORAGE_DEVICE=$(lsblk -o NAME,TYPE | grep disk | awk 'NR==1{print "/dev/"$1}')
+echo "Detected storage device: $STORAGE_DEVICE"
 
-# Create a virtual machine image
-echo "Creating a disk image for MikroTik CHR..."
-qemu-img create -f qcow2 chr.qcow2 10G
+# Set root password (change to your desired method for setting the password)
+ROOT_PASSWORD="your_secure_password_here"
+echo "Using root password: $ROOT_PASSWORD"
 
-# Convert MikroTik CHR raw image to qcow2 format
-echo "Converting CHR image to qcow2..."
-qemu-img convert chr-$CHR_VERSION.img -O qcow2 chr.qcow2
+# Download MikroTik RouterOS image
+wget https://download.mikrotik.com/routeros/7.5/chr-7.5.img.zip -O chr.img.zip && \
 
-# Set up a virtual machine using KVM
-echo "Setting up the MikroTik CHR virtual machine..."
-virt-install --name mikrotik-chr --ram 512 --vcpus 1 --disk path=chr.qcow2,format=qcow2,bus=virtio \
---import --network network=default,model=virtio --os-type linux --os-variant generic --graphics none \
---noautoconsole --cpu host
+# Extract the image
+gunzip -c chr.img.zip > chr.img && \
 
-# Clean up unnecessary files
-echo "Cleaning up downloaded files..."
-rm chr.img.zip chr-$CHR_VERSION.img
+# Mount the image
+mount -o loop,offset=512 chr.img /mnt && \
 
-echo "MikroTik CHR installation completed!"
+# Create the MikroTik configuration script
+echo "/ip address add address=$ADDRESS interface=[/interface ethernet find where name=ether1]
+/ip route add gateway=$GATEWAY
+/ip service disable telnet
+/user set 0 name=root password=$ROOT_PASSWORD" > /mnt/rw/autorun.scr && \
 
-# Force reboot the server
-sudo reboot -f
+# Sync and install RouterOS on the detected storage device
+echo u > /proc/sysrq-trigger && \
+dd if=chr.img bs=1024 of=$STORAGE_DEVICE && \
+echo "sync disk" && \
+echo s > /proc/sysrq-trigger && \
+echo "Sleep 5 seconds" && \
+sleep 5 && \
+echo "Ok, reboot" && \
+echo b > /proc/sysrq-trigger
